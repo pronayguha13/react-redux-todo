fc14dca38ffdda611c0c3a250da3492c97af9a6c
Pronay Guha <pronayguha13@gmail.com>
2021-10-28 23:14:04 +0530

    removed unsued files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import Navbar from "./Component/Navbar";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Navbar />
       </PersistGate>
     </Provider>
   );
