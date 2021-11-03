fbc560af12b7fc1ae39eed4a4651afb4aab7126d
Pronay Guha <pronayguha13@gmail.com>
2021-11-04 01:14:13 +0530

    started designing navbar

--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Col, Row } from "antd";
+
+import styles from "./styles.module.css";
 
 const Navbar = () => {
-  return <div>This is the navbar</div>;
+  return (
+    <Row justify="space-between" align="middle" className={styles.container}>
+      <Col span={4}>Logo</Col>
+      <Col span={12}>Controller section</Col>
+    </Row>
+  );
 };
 
 export default Navbar;
